44d027f6347030c4cd9b55d5f1a9d8231fd5aa05
Stanley <angstanley68@gmail.com>
2021-02-15 19:54:36 +0700

    Create tableview

---
+++ mercury/DefinitionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DefinitionTableViewCell.swift
+//  mercury
+//
+//  Created by Stanley on 17/02/21.
+//
+
+import UIKit
+
+class DefinitionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- mercury/ViewController.swift
+++ mercury/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import SkeletonView
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DefinitionCell", for: indexPath)
+        return cell
+    }
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 300
+        tableView.isHidden = true
+
     }
 
 
